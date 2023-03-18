6366ad42ce9bbbc967f2afbc06b4a997d8d4fde0
Evgeniy <241126@mail.ru>
2023-03-19 00:43:15 +0300

    sec comm

--- Sokket.py
+++ Sokket.py
@@ -1,27 +1,25 @@
-import socket
-import threading
+import socket, threading
 from time import sleep
 
-sock = socket.socket()
-addr = ('87.250.250.242', 443)
-sock.connect(addr)
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 
+sock = socket.socket()
+sock.connect((HOST, PORT))
+sock.sendall(b"Hello, world")
 def sock_send(data):
     sock.send(data)
 
 def sock_recieve():
     while True:
         data_in = sock.recv(1024)
-        print(data_in.encode('ascii'))
+        print(data_in.decode('ascii'))
 
-sock_send(b'evgenii')
-
-
-rec_thread = threading.Thread (target = sock_recieve)
+rec_thread = threading.Thread(target=sock_recieve)
 rec_thread.start()
 
 while True:
     data = input()
-    sock_send(f'evgenii: {data}'.encode('ascii'))
+    sock_send(f"Evgenii_client: {data}".encode('ascii'))
 
 # 127,0,0,1, 11111
